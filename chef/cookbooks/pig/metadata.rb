efa02924845d1e52f8749b4108d74751750f526d
Paul Webster <Paul_Webster@Dell.com>
2011-09-02 18:22:45 -0400

    Hadoop PIG subproject - inital checkin of the PIG barclamp. Still needs the following adjustments; a) JAVA_HOME needs to be set in the shell env before executing the "pig" executable. b) Still need to add code to the barclamp controller to attach it to the hadoop    edge node.

---
+++ chef/cookbooks/pig/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Dell, Inc."
+maintainer_email "Paul_Webster@Dell.com"
+license          "All rights reserved"
+description      "A high-level data-flow language and execution framework for parallel computation"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "1.0"
+recipe           "pig::default", "Installs pig base libraries and configuration."
