17b126c641969747bea157daca97f0c6c8f3ff67
Paul Webster <Paul_Webster@Dell.com>
2011-09-06 18:03:19 -0400

    DE291 Fixed - Hive incorrectly escaped value. Added HAML code to render the custom attribute and deployment widgets. Added the model hive_service.rb code to bind the proposal to edge nodes. Note - This barclamp is still in a dev testing state. Please test - but expect some issues.

--- chef/cookbooks/pig/metadata.rb
+++ chef/cookbooks/pig/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Dell, Inc."
 maintainer_email "Paul_Webster@Dell.com"
-license          "All rights reserved"
+license          "Apache 2.0 License, Copyright (c) 2011 Dell Inc. - http://www.apache.org/licenses/LICENSE-2.0"
 description      "A high-level data-flow language and execution framework for parallel computation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0"
