87406bbf89914afb64518e8858ec4107298e2f85
Paul Webster <Paul_Webster@Dell.com>
2011-09-08 06:51:18 -0400

    Initial checkin of the Hadoop ZooKeeper barclamp.

--- chef/cookbooks/pig/metadata.rb
+++ chef/cookbooks/pig/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Dell, Inc."
 maintainer_email "Paul_Webster@Dell.com"
 license          "Apache 2.0 License, Copyright (c) 2011 Dell Inc. - http://www.apache.org/licenses/LICENSE-2.0"
-description      "A platform for analyzing large data sets that consists of a high-level language for expressing data algorithms."
+description      "Platform for analyzing large data sets that consists of a high-level language for expressing data algorithms."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0"
 recipe           "pig::default", "Installs pig base libraries and configuration."
